3ae0eb2c7a256db0672e29eff1f1f7f31be33236
Junming Zhao <u6633756@anu.edu.au>
2019-09-07 00:59:30 +1000

    Initialization

---
+++ app/src/main/java/cs/anu/edu/au/comp2100/weiming/MainActivity.java
@@ -0,0 +1,15 @@
+package cs.anu.edu.au.comp2100.weiming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
