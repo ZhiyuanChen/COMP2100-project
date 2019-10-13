e24408397cd9f3580bc3fb679227afdc9065e77f
Zhiyuan Chen <zc@int.ac.cn>
2019-10-13 15:42:18 +1100

    Update defintion of objects

--- app/src/main/java/cs/anu/edu/au/comp2100/weiming/object/Schedule.java
+++ app/src/main/java/cs/anu/edu/au/comp2100/weiming/object/Schedule.java
@@ -1,5 +1,7 @@
 package cs.anu.edu.au.comp2100.weiming.object;
 
+import android.location.Location;
+
 import java.util.Calendar;
 
 public class Schedule {
@@ -7,6 +9,7 @@ public class Schedule {
   private Category category;
   private Calendar start_time;
   private Calendar end_time;
+  private Location location;
   private String description;
 
   public Schedule(
@@ -14,11 +17,56 @@ public class Schedule {
       Category category,
       Calendar start_time,
       Calendar end_time,
+      Location location,
       String description) {
     this.course = course;
     this.category = category;
     this.start_time = start_time;
     this.end_time = end_time;
+    this.location = location;
+    this.description = description;
+  }
+
+  public Schedule(
+          Course course,
+          Category category,
+          Calendar start_time,
+          Calendar end_time,
+          String description) {
+    this.course = course;
+    this.category = category;
+    this.start_time = start_time;
+    this.end_time = end_time;
+    this.description = description;
+  }
+
+  public Schedule(
+          Course course,
+          Category category,
+          Calendar start_time,
+          int interval,
+          Location location,
+          String description) {
+    this.course = course;
+    this.category = category;
+    this.start_time = start_time;
+    this.end_time = Calendar.getInstance();
+    this.end_time.setTimeInMillis(this.start_time.getTimeInMillis() + interval * 3600000);
+    this.location = location;
+    this.description = description;
+  }
+
+  public Schedule(
+          Course course,
+          Category category,
+          Calendar start_time,
+          int interval,
+          String description) {
+    this.course = course;
+    this.category = category;
+    this.start_time = start_time;
+    this.end_time = Calendar.getInstance();
+    this.end_time.setTimeInMillis(this.start_time.getTimeInMillis() + interval * 3600000);
     this.description = description;
   }
 
